e33e210d4e0eb96f565089c83ffc113df084773f
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2023-11-25 18:28:38 -0300

    Refactor user controller and routes

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,32 +1,11 @@
 const User = require("../models/user.model")
 
-const userController = {
-	// Get all users
-	getUsers: async (req, res) => {
-		const users = await User.find({})
-		res.json(users)
-	},
-
-	// Create a new user
-	createUser: async (req, res, next) => {
-		const { name, email, password } = req.body
-
-		try {
-			const user = await User.create({ name, email, password })
-
-			res.status(201).json(user)
-			console.log(user)
-		} catch (error) {
-			if (error.code === 11000) {
-				// Error de duplicado (clave única)
-				res.status(400).json({ error: "El usuario ya existe" })
-			} else if (error.name === "ValidationError") {
-				res.status(400).json({ error: error.message })
-			} else {
-				next(error)
-			}
-		}
-	},
+// Get all users
+const getUsers = async (req, res) => {
+	const users = await User.find({})
+	res.json(users)
 }
 
-module.exports = userController
+module.exports = {
+	getUsers,
+}
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -6,7 +6,6 @@ const UserController = require("../controllers/user.controller")
 // Routes
 router.get("/", UserController.getUsers)
 //* router.get("/:id", UserController.getUserById)
-router.post("/", UserController.createUser)
 //* router.put("/:id", UserController.updateUser)
 //* router.delete("/:id", UserController.deleteUser)
 
