9a4fb9fe371698ab675535fc4d92a30b751808fc
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2023-11-27 16:27:54 -0300

    Add NextUI and Framer Motion dependencies, import Post component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react"
+import Post from "./components/Post"
 
 function App() {
 	return (
 		<>
 			<h1>Vite + React</h1>
+			<Post />
 		</>
 	)
 }
---
+++ frontend/src/components/Post.jsx
@@ -0,0 +1,13 @@
+import { Avatar, AvatarGroup, AvatarIcon } from "@nextui-org/react"
+
+function Post() {
+	return (
+		<div>
+			<img
+				src="https://picsum.photos/200/300"
+				alt="random"
+			/>
+		</div>
+	)
+}
+export default Post
